b19b775f4f3874d2bb6591d947ffee3b7ff95207
Darshan Patil <darshanpatil1845@gmail.com>
2026-07-24 21:28:56 +0530

    Disable Flask debug mode in app.py
    Commented out the app.run() line to disable debug mode.

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def index():
 
     return render_template('result.html', result=y_pred)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
